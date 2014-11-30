158eff742c9006351572ccb543ef6b05aa046040
xXero <xerozus.mlg@gmail.com>
2014-11-30 17:41:47 +0100

    xFizz release

---
+++ xFizz/xFizz/Program.cs
@@ -0,0 +1,317 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LeagueSharp;
+using LeagueSharp.Common;
+using SharpDX;
+using Color = System.Drawing.Color;
+namespace xFizz
+{
+    class Program
+    {
+        public static string ChampName = "Fizz";
+        public static Orbwalking.Orbwalker Orbwalker;
+        private static readonly Obj_AI_Hero player = ObjectManager.Player;
+        public static Spell Q, W, E, E2,R;
+        public static SpellSlot IgniteSlot;
+        public static Items.Item Dfg;
+      
+
+        static void Main(string[] args)
+        {
+            CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
+        }
+        public static Menu xMenu;
+
+
+        private static void Game_OnGameLoad(EventArgs args)
+        {
+            if (player.BaseSkinName != ChampName) return;
+
+            Q = new Spell(SpellSlot.Q, 550);
+            W = new Spell(SpellSlot.W, 0);
+            E = new Spell(SpellSlot.E, 400);
+            E2 = new Spell(SpellSlot.E, 400);
+            R = new Spell(SpellSlot.R, 1200);
+
+            E.SetSkillshot(0.5f, 120, 1300, false, SkillshotType.SkillshotCircle);
+            E2.SetSkillshot(0.5f, 400, 1300, false, SkillshotType.SkillshotCircle);
+            R.SetSkillshot(0.5f, 250f, 1200f, false, SkillshotType.SkillshotLine);
+
+            IgniteSlot = player.GetSpellSlot("SummonerDot");
+
+            Dfg = new Items.Item(3128, 750f);
+            
+
+            xMenu = new Menu("x" + ChampName, ChampName, true);
+
+            xMenu.AddSubMenu(new Menu("Orbwalker", "Orbwalker"));
+            Orbwalker = new Orbwalking.Orbwalker(xMenu.SubMenu("Orbwalker"));
+
+            var ts = new Menu("Target Selector", "Target Selector");
+            SimpleTs.AddToMenu(ts);
+            xMenu.AddSubMenu(ts);
+
+            xMenu.AddSubMenu(new Menu("Combo", "Combo"));
+            xMenu.SubMenu("Combo").AddItem(new MenuItem("useQ", "Use Q?").SetValue(true));
+            xMenu.SubMenu("Combo").AddItem(new MenuItem("useW", "Use W?").SetValue(true));
+            xMenu.SubMenu("Combo").AddItem(new MenuItem("useE", "Use E?").SetValue(true));
+            xMenu.SubMenu("Combo").AddItem(new MenuItem("useR", "Use R?").SetValue(true));
+            xMenu.SubMenu("Combo").AddItem(new MenuItem("useItems", "Use Items?").SetValue(true));
+            xMenu.SubMenu("Combo").AddItem(new MenuItem("ComboActive", "Combo").SetValue(new KeyBind(32, KeyBindType.Press)));
+
+            xMenu.AddSubMenu(new Menu("Harass", "Harass"));
+            xMenu.SubMenu("Harass").AddItem(new MenuItem("hQ", "Use Q?").SetValue(true));
+            xMenu.SubMenu("Harass").AddItem(new MenuItem("hE", "Use E?").SetValue(true));
+            xMenu.SubMenu("Harass").AddItem(new MenuItem("Harassmana", "Mana to use").SetValue(new Slider(30)));
+            xMenu.SubMenu("Harass").AddItem(new MenuItem("HarassActive", "Harass").SetValue(new KeyBind('C', KeyBindType.Press)));
+            xMenu.SubMenu("Harass").AddItem(new MenuItem("HarassToggle", "Harass").SetValue(new KeyBind('T', KeyBindType.Toggle)));
+
+            xMenu.AddSubMenu(new Menu("Killsteal", "Killsteal"));
+            xMenu.SubMenu("Killsteal").AddItem(new MenuItem("KillQ", "Steal with Q?").SetValue(true));
+            xMenu.SubMenu("Killsteal").AddItem(new MenuItem("KillE", "Steal with E?").SetValue(true));
+            xMenu.SubMenu("Killsteal").AddItem(new MenuItem("KillR", "Steal with R?").SetValue(true));
+            xMenu.SubMenu("Killsteal").AddItem(new MenuItem("KillI", "Steal with Ignite?").SetValue(true));
+
+            xMenu.AddSubMenu(new Menu("Drawing", "Drawing"));
+            xMenu.SubMenu("Drawing").AddItem(new MenuItem("DrawQ", "Draw Q?").SetValue(true));
+            xMenu.SubMenu("Drawing").AddItem(new MenuItem("DrawE", "Draw E?").SetValue(true));
+            xMenu.SubMenu("Drawing").AddItem(new MenuItem("DrawAA", "Draw Range?").SetValue(true));
+
+
+
+
+            xMenu.AddItem(new MenuItem("Packet", "Packet Casting").SetValue(true));
+
+            xMenu.AddToMainMenu();
+
+            Drawing.OnDraw += Drawing_OnDraw;
+            Game.OnGameUpdate += Game_OnGameUpdate;
+
+            Game.PrintChat("x" + ChampName);
+        }
+
+        static void Game_OnGameUpdate(EventArgs args)
+        {
+            if (xMenu.Item("ComboActive").GetValue<KeyBind>().Active)
+            {
+                Combo();
+            }
+
+            if (xMenu.Item("HarassActive").GetValue<KeyBind>().Active || xMenu.Item("HarassToggle").GetValue<KeyBind>().Active)
+            {
+                Harass();
+            }
+
+            KillSteal();
+        }
+
+
+        private static float GetIgniteDamage(Obj_AI_Hero enemy)
+        {
+            if (IgniteSlot == SpellSlot.Unknown || player.SummonerSpellbook.CanUseSpell(IgniteSlot) != SpellState.Ready) return 0f;
+            return (float)player.GetSummonerSpellDamage(enemy, Damage.SummonerSpell.Ignite);
+        }
+
+        static void Drawing_OnDraw(EventArgs args)
+        {
+            if (xMenu.Item("DrawQ").GetValue<bool>() == true)
+            {
+                Utility.DrawCircle(player.Position, Q.Range, Color.Red);
+            }
+
+            if (xMenu.Item("DrawE").GetValue<bool>() == true)
+            {
+                Utility.DrawCircle(player.Position, E.Range, Color.Orange);
+            }
+            if (xMenu.Item("DrawAA").GetValue<bool>() == true)
+            {
+                Utility.DrawCircle(player.Position, player.AttackRange, Color.Blue);
+            }
+
+
+
+
+
+        }
+
+        public static void KillSteal()
+        {
+            var target = SimpleTs.GetTarget(Q.Range, SimpleTs.DamageType.Magical);
+            if (target == null) return;
+
+            if (target.IsValidTarget(Q.Range) && Q.IsReady() && xMenu.Item("KillQ").GetValue<bool>() == true && ObjectManager.Player.GetSpellDamage(target, SpellSlot.Q) > target.Health)
+            {
+                Q.Cast(target, xMenu.Item("Packet").GetValue<bool>());
+            }
+
+            if (target.IsValidTarget(E.Range) && E.IsReady() && xMenu.Item("KillE").GetValue<bool>() == true && ObjectManager.Player.GetSpellDamage(target, SpellSlot.E-100) + player.GetSpellDamage(target, SpellSlot.E)> target.Health)
+            {
+                E.Cast(target, xMenu.Item("Packet").GetValue<bool>());
+                E2.Cast(target);
+            }
+
+            if (target.IsValidTarget(R.Range) && R.IsReady() && xMenu.Item("KillR").GetValue<bool>() && player.GetSpellDamage(target, SpellSlot.R) > target.Health +20)
+
+           
+
+
+            if (xMenu.Item("KillI").GetValue<bool>() == true)
+            {
+                if (IgniteSlot != SpellSlot.Unknown &&
+                    player.SummonerSpellbook.CanUseSpell(IgniteSlot) == SpellState.Ready)
+                {
+                    if (target.Health + 20 <= GetIgniteDamage(target))
+                    {
+                        player.SummonerSpellbook.CastSpell(IgniteSlot, target);
+                    }
+                }
+
+            }
+
+
+
+
+        }
+
+
+
+        public static void Harass()
+        {
+            if (player.Mana / player.MaxMana * 100 > xMenu.SubMenu("Harass").Item("Harassmana").GetValue<Slider>().Value)
+                return;
+
+
+            var target = SimpleTs.GetTarget(Q.Range, SimpleTs.DamageType.Magical);
+            if (target == null)
+                return;
+            {
+                if (target.IsValidTarget(Q.Range) && Q.IsReady() && xMenu.Item("hQ").GetValue<bool>() == true)
+                {
+                    Q.Cast(target, xMenu.Item("Packet").GetValue<bool>());
+                }
+
+                if (target.IsValidTarget(E.Range) && E.IsReady() && xMenu.Item("hE").GetValue<bool>() == true)
+                {
+                    E.Cast(target, xMenu.Item("Packet").GetValue<bool>());
+                    E2.Cast(target);
+                }
+
+
+            }
+
+
+
+
+
+
+        }
+        private static float ComboDamage(Obj_AI_Base enemy)
+        {
+            double damage = 0d;
+
+            if (Dfg.IsReady())
+                damage += player.GetItemDamage(enemy, Damage.DamageItems.Dfg) / 1.2;
+
+            if (Q.IsReady())
+                damage += player.GetSpellDamage(enemy, SpellSlot.Q);
+
+            if (R.IsReady())
+                damage += player.GetSpellDamage(enemy, SpellSlot.R);
+
+            if (Dfg.IsReady())
+                damage = damage * 1.2;
+
+            if (E.IsReady())
+                damage += player.GetSpellDamage(enemy, SpellSlot.E);
+
+            if (IgniteSlot != SpellSlot.Unknown && player.SummonerSpellbook.CanUseSpell(IgniteSlot) == SpellState.Ready)
+                damage += ObjectManager.Player.GetSummonerSpellDamage(enemy, Damage.SummonerSpell.Ignite);
+
+            if (Items.HasItem(3155, (Obj_AI_Hero)enemy))
+            {
+                damage = damage - 250;
+            }
+
+            if (Items.HasItem(3156, (Obj_AI_Hero)enemy))
+            {
+                damage = damage - 400;
+            }
+            return (float)damage;
+        }
+
+        public static void Combo()
+        {
+            var target = SimpleTs.GetTarget(Q.Range, SimpleTs.DamageType.Magical);
+            if (target == null) return;
+            float dmg = ComboDamage(target);
+
+            if (dmg > target.Health + +20)
+            {
+                    if (Dfg.IsReady() && xMenu.Item("useItems").GetValue<bool>() == true)
+                     {
+                            Dfg.Cast(target);
+                         }
+                if (target.IsValidTarget(R.Range) && R.IsReady())
+            {
+                R.Cast(target, xMenu.Item("Packet").GetValue<bool>());
+
+            }
+                if (target.IsValidTarget(W.Range) && W.IsReady() && xMenu.Item("useW").GetValue<bool>() == true)
+            {
+                W.Cast(target, xMenu.Item("Packet").GetValue<bool>());
+            }
+                if (target.IsValidTarget(Q.Range) && Q.IsReady() && xMenu.Item("useQ").GetValue<bool>() == true)
+            {
+                Q.CastOnUnit(target, xMenu.Item("Packet").GetValue<bool>());
+
+
+
+            }
+                if (target.IsValidTarget(E.Range) && E.IsReady() && xMenu.Item("useE").GetValue<bool>() == true)
+                {
+                    E.Cast(target, xMenu.Item("Packet").GetValue<bool>());
+                    E2.Cast(target);
+                }
+
+            }
+            else
+            {
+                if (target.IsValidTarget(Q.Range) && Q.IsReady() && xMenu.Item("useQ").GetValue<bool>() == true)
+                {
+                    Q.CastOnUnit(target, xMenu.Item("Packet").GetValue<bool>());
+
+
+
+                }
+                if (target.IsValidTarget(E.Range) && E.IsReady() && xMenu.Item("useE").GetValue<bool>() == true)
+                {
+                    E.Cast(target, xMenu.Item("Packet").GetValue<bool>());
+                    E2.Cast(target);
+                }
+            }
+          
+
+           
+
+            
+
+            
+
+
+
+           
+
+
+
+
+
+
+
+
+        }
+    }
+}
