744bb18c5c9074501ce8154461fef032b22657c1
xXero <xerozus.mlg@gmail.com>
2014-11-30 15:05:43 +0100

    xWard fixed bugs

--- xWard/xWard/Program.cs
+++ xWard/xWard/Program.cs
@@ -68,45 +68,47 @@ namespace xWard
         {
             
             var circleRange = 125f;
+            if (xMenu.Item("Draww").GetValue<bool>())
+            {
+                Utility.DrawCircle(new Vector3(2524, 10406, 54f), circleRange, Color.Blue);
+                Utility.DrawCircle(new Vector3(1774, 10756, 52f), circleRange, Color.Blue);
+                Utility.DrawCircle(new Vector3(5520, 6342, 51f), circleRange, Color.Blue);
+                Utility.DrawCircle(new Vector3(5674, 7358, 51f), circleRange, Color.Blue);
+                Utility.DrawCircle(new Vector3(7990, 4282, 53f), circleRange, Color.Blue);
+                Utility.DrawCircle(new Vector3(8256, 2920, 51f), circleRange, Color.Blue);
+                Utility.DrawCircle(new Vector3(4818, 10866, -71f), circleRange, Color.Blue);
+                Utility.DrawCircle(new Vector3(6824, 10656, 55f), circleRange, Color.Blue);
+                Utility.DrawCircle(new Vector3(6574, 12006, 56f), circleRange, Color.Blue);
+                Utility.DrawCircle(new Vector3(9130, 8346, 53f), circleRange, Color.Blue);
+                Utility.DrawCircle(new Vector3(9422, 7408, 52f), circleRange, Color.Blue);
+                Utility.DrawCircle(new Vector3(12372, 4508, 51f), circleRange, Color.Blue);
+                Utility.DrawCircle(new Vector3(13003, 3818, 51f), circleRange, Color.Blue);
 
-            Utility.DrawCircle(new Vector3(2524, 10406, 54f), circleRange, Color.Blue);
-            Utility.DrawCircle(new Vector3(1774, 10756, 52f), circleRange, Color.Blue);
-            Utility.DrawCircle(new Vector3(5520, 6342, 51f), circleRange, Color.Blue);
-            Utility.DrawCircle(new Vector3(5674, 7358, 51f), circleRange, Color.Blue);
-            Utility.DrawCircle(new Vector3(7990, 4282, 53f), circleRange, Color.Blue);
-            Utility.DrawCircle(new Vector3(8256, 2920, 51f), circleRange, Color.Blue);
-            Utility.DrawCircle(new Vector3(4818, 10866, -71f), circleRange, Color.Blue);
-            Utility.DrawCircle(new Vector3(6824, 10656, 55f), circleRange, Color.Blue);
-            Utility.DrawCircle(new Vector3(6574, 12006, 56f), circleRange, Color.Blue);
-            Utility.DrawCircle(new Vector3(9130, 8346, 53f), circleRange, Color.Blue);
-            Utility.DrawCircle(new Vector3(9422, 7408, 52f), circleRange, Color.Blue);
-            Utility.DrawCircle(new Vector3(12372, 4508, 51f), circleRange, Color.Blue);
-            Utility.DrawCircle(new Vector3(13003, 3818, 51f), circleRange, Color.Blue);
 
 
-
-            Utility.DrawCircle(new Vector3(2729, 10879, -71f), circleRange, Color.Green);
-            Utility.DrawCircle(new Vector3(2303, 10868, 53f), circleRange, Color.Green);
-            Utility.DrawCircle(new Vector3(5223, 6789, 50f), circleRange, Color.Green);
-            Utility.DrawCircle(new Vector3(5191, 7137, 50f), circleRange, Color.Green);
-            Utility.DrawCircle(new Vector3(8368, 4594, 51f), circleRange, Color.Green);
-            Utility.DrawCircle(new Vector3(8100, 3429, 51f), circleRange, Color.Green);
-            Utility.DrawCircle(new Vector3(4634, 11283, 49f), circleRange, Color.Green);
-            Utility.DrawCircle(new Vector3(6672, 11466, 53f), circleRange, Color.Green);
-            Utility.DrawCircle(new Vector3(6518, 10367, 53f), circleRange, Color.Green);
-            Utility.DrawCircle(new Vector3(9572, 8038, 57f), circleRange, Color.Green);
-            Utility.DrawCircle(new Vector3(9697, 7854, 51f), circleRange, Color.Green);
-            Utility.DrawCircle(new Vector3(12235, 4068, -68f), circleRange, Color.Green);
-            Utility.DrawCircle(new Vector3(12443, 4021, -7f), circleRange, Color.Green); 
-                
+                Utility.DrawCircle(new Vector3(2729, 10879, -71f), circleRange, Color.Green);
+                Utility.DrawCircle(new Vector3(2303, 10868, 53f), circleRange, Color.Green);
+                Utility.DrawCircle(new Vector3(5223, 6789, 50f), circleRange, Color.Green);
+                Utility.DrawCircle(new Vector3(5191, 7137, 50f), circleRange, Color.Green);
+                Utility.DrawCircle(new Vector3(8368, 4594, 51f), circleRange, Color.Green);
+                Utility.DrawCircle(new Vector3(8100, 3429, 51f), circleRange, Color.Green);
+                Utility.DrawCircle(new Vector3(4634, 11283, 49f), circleRange, Color.Green);
+                Utility.DrawCircle(new Vector3(6672, 11466, 53f), circleRange, Color.Green);
+                Utility.DrawCircle(new Vector3(6518, 10367, 53f), circleRange, Color.Green);
+                Utility.DrawCircle(new Vector3(9572, 8038, 57f), circleRange, Color.Green);
+                Utility.DrawCircle(new Vector3(9697, 7854, 51f), circleRange, Color.Green);
+                Utility.DrawCircle(new Vector3(12235, 4068, -68f), circleRange, Color.Green);
+                Utility.DrawCircle(new Vector3(12443, 4021, -7f), circleRange, Color.Green);
+            }
 
             }
 
         private static void PlaceW()
         {
-            if (TrinketN.IsReady() || SightStone.IsReady() || WardS.IsReady() || WardN.IsReady())
-            {
+           if (TrinketN.IsReady())
+           {
                 TrinketN.Cast(new Vector3(2729, 10879, -71f));
+                
                 TrinketN.Cast(new Vector3(2303, 10868, 53f));
                 TrinketN.Cast(new Vector3(5223, 6789, 50f));
                 TrinketN.Cast(new Vector3(5191, 7137, 50f));
@@ -119,6 +121,11 @@ namespace xWard
                 TrinketN.Cast(new Vector3(9697, 7854, 51f));
                 TrinketN.Cast(new Vector3(12235, 4068, -68f));
                TrinketN.Cast(new Vector3(12443, 4021, -7f));
+               return;
+           }
+
+            if (SightStone.IsReady())
+            {
                 SightStone.Cast(new Vector3(2729, 10879, -71f));
                 SightStone.Cast(new Vector3(2303, 10868, 53f));
                 SightStone.Cast(new Vector3(5223, 6789, 50f));
@@ -132,6 +139,11 @@ namespace xWard
                 SightStone.Cast(new Vector3(9697, 7854, 51f));
                 SightStone.Cast(new Vector3(12235, 4068, -68f));
                 SightStone.Cast(new Vector3(12443, 4021, -7f));
+                return;
+            }
+
+            if (WardS.IsReady())
+            {
                 WardS.Cast(new Vector3(2729, 10879, -71f));
                 WardS.Cast(new Vector3(2303, 10868, 53f));
                 WardS.Cast(new Vector3(5223, 6789, 50f));
@@ -145,6 +157,10 @@ namespace xWard
                 WardS.Cast(new Vector3(9697, 7854, 51f));
                 WardS.Cast(new Vector3(12235, 4068, -68f));
                 WardS.Cast(new Vector3(12443, 4021, -7f));
+                return;
+            }
+            if (WardN.IsReady())
+            {
                 WardN.Cast(new Vector3(2729, 10879, -71f));
                 WardN.Cast(new Vector3(2303, 10868, 53f));
                 WardN.Cast(new Vector3(5223, 6789, 50f));
@@ -158,6 +174,7 @@ namespace xWard
                 WardN.Cast(new Vector3(9697, 7854, 51f));
                 WardN.Cast(new Vector3(12235, 4068, -68f));
                 WardN.Cast(new Vector3(12443, 4021, -7f));
+                return;
             }
 
 
